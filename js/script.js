da038fd36ba88dd000fce6e28af39e09c6a99e5c
George Heavenson Jnr. Oti-Adjei <jrgeorge991@outlook.com>
2023-02-12 23:44:16 +0000

    Clean commit

--- js/script.js
+++ js/script.js
@@ -2,30 +2,23 @@ const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
 	header.classList.toggle("sticky", window.scrollY > 0);
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('active');
 });
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-// menu.onclick = () => {
-// 	// menu.classList.toggle('bx-x');
-// 	navlist.classList.toggle('active');
-// };
-
-window.onscroll = () => {
-	menu.classList.remove('bx-x');
-	navlist.classList.remove('active');
+menu.onclick = () => {
+	 menu.classList.toggle('bx-x');
+	navlist.classList.toggle('active');
 };
-window.sr = ScrollReveal({
-	distance: '45px',
-	duration: 2700, reset: true
-});
 
-// const sr = ScrollReveal({
-// 	distance: '45px',
-// 	duration: 2700,
-// 	reset: true
-// })
+const sr = ScrollReveal({
+	distance: '45px',
+	duration: 2700,
+	reset: true
+})
 
 sr.reveal('.home-text', { delay: 350, origin: 'left' })
 sr.reveal('.home-img', { delay: 350, origin: 'right' })
