3a837b47440f8de152ec98dc5f04b2fc1511f381
George Heavenson Jnr. Oti-Adjei <jrgeorge991@outlook.com>
2023-12-30 01:34:28 +0000

    Playing with color schemes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 //Making variables and assigning html classes to it so as to use it
 
 const header = document.querySelector("header");
-
-
-//This function is to check if the scroll is greater than 0, 
-// if yes let the header be sticky and the x button and navlist should not show whles scrolling
-// window.scroll is a property that represents the number of pixels that the document has been scrolled vertically. 
-window.addEventListener("scroll", function () {
-	header.classList.toggle("sticky", window.scrollY > 0);
-	menu.classList.remove('bx-x');
-	navlist.classList.remove('active');
-});
-
-//Making variables and assigning html classes to it so as to use it
-
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 const homeimage = document.querySelector(".home");
 const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 
+// * Make navbar sticky
+//This function is to check if the scroll is greater than 0, if yes let the header be sticky and the x button and navlist should not show whles scrolling
+window.addEventListener("scroll", function () {
+	header.classList.toggle("sticky", window.scrollY > 0);
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('active');
+});
 
+
+// * Make hamburger menu open navlist
 //  This function is to check if the menu icon button on smaller screen is pressed 
 // if pressed it  should chage to the x button
 menu.onclick = () => {
@@ -28,58 +24,19 @@ menu.onclick = () => {
 	navlist.classList.toggle('active');
 };
 
-// Function to toggle dark mode and moon image
-function toggleDarkMode() {
-	const body = document.body;
-	const moonImage = document.querySelector('#dark-mode-toggle img');
-  
-	// Check if dark mode is currently active using media query
-	const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  
-	if (isDarkMode) {
-	  // Apply light mode styles (remove any dark mode overrides)
-	  moonImage.src = './assets/images/moon.png'; // Change to full moon image
-	} else {
-	  // Apply dark mode overrides
-	  moonImage.src = './assets/images/crescent-moon-phase.png'; // Change to crescent moon image
-	}
-  }
-  
-  // Add event listener to the button (no changes here)
-  const toggleButton = document.getElementById('dark-mode-toggle');
-  toggleButton.addEventListener('click', toggleDarkMode);
-  
-  // Initially check for user preference and apply dark mode overrides if needed
-  if (prefersDark) {
-	toggleDarkMode(); // Apply crescent moon image initially if preferred
-  }
-
-  // Light/Dark Mode
-
-const body = document.querySelector("body");
-const toggle = document.querySelector("#toggle");
-const sunIcon = document.querySelector(".toggle .bxs-sun");
-const moonIcon = document.querySelector(".toggle .bx-moon");
-
-toggle.addEventListener("change", () => {
-    body.classList.toggle("dark");
-    sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
-    moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
-});
-  
 
 
 
+// * Animation function using scrol reveal framework
 
-//animation function using scrol reveal framework
 const sr = ScrollReveal({
 	distance: '45px',
 	duration: 2700,
 	reset: true
 })
-
 sr.reveal('.home-text', { delay: 350, origin: 'left' })
 sr.reveal('.home-img', { delay: 350, origin: 'right' })
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', { delay: 100, origin: 'bottom' })
 // Check for initial user preference
 if (prefersDark) {
 	// body.classList.add('dark-mode');
@@ -88,8 +45,11 @@ if (prefersDark) {
 	sr.reveal('.home', { delay: 100, origin: 'right' })
 }
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', { delay: 100, origin: 'bottom' })
+//* End of Animation funciton
 
+
+
+//* Modal section
 
 let moonbutton = document.querySelector('.profile');
 moonbutton.onclick = () => {
@@ -106,10 +66,6 @@ const closeBtn = document.getElementsByClassName("closeBtn")[0];
 openModalBtn.onclick = () => {
 	myModal.style.display = "block";
 };
-//if i use event listener the moonbutton will excute since i used used eventlistner but since i used on click rather it will override.
-// openModalBtn.addEventListener("click", function() {
-// 	myModal.style.display = "block";
-// });
 
 closeBtn.addEventListener("click", function () {
 	myModal.style.display = "none";
@@ -120,4 +76,3 @@ window.addEventListener("click", function (event) {
 		myModal.style.display = "none";
 	}
 });
-
